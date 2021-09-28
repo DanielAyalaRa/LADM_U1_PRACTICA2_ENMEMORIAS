09a50ffcba38ad33be5e6c8d7a9a6eead39851e1
DanielAyalaRa <daalayalar@ittepic.edu.mx>
2021-09-27 23:58:59 -0600

    practica2

---
+++ LADM_U1_Practica2_ArchivosMemoria/app/src/main/java/mx/tecm/tepic/ladm_u1_practica2_archivosmemoria/MainActivity2.kt
@@ -0,0 +1,11 @@
+package mx.tecm.tepic.ladm_u1_practica2_archivosmemoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
---
+++ LADM_U1_Practica2_ArchivosMemoria/app/src/main/java/mx/tecm/tepic/ladm_u1_practica2_archivosmemoria/SplashScreenActivity.kt
@@ -0,0 +1,11 @@
+package mx.tecm.tepic.ladm_u1_practica2_archivosmemoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
